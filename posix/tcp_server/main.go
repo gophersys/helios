adfcebb68c46efd0061e4bae65562137e08683f4
Mateo Segura <mateo@corekinect.com>
2023-09-15 21:17:18 +0000

    added downlink handshake to go program

--- posix/tcp_server/main.go
+++ posix/tcp_server/main.go
@@ -1,25 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"encoding/binary"
+	"log"
 	"net"
 	"os"
 )
 
+const ServerProtocolVersion = 1
+
 func main() {
 	listen, err := net.Listen("tcp", ":6969")
 	if err != nil {
-		fmt.Printf("Failed to listen on port 6969: %v\n", err)
+		log.Printf("Failed to listen on port 6969: %v\n", err)
 		os.Exit(1)
 	}
 	defer listen.Close()
 
-	fmt.Println("Listening on port 6969...")
+	log.Println("Listening on port 6969...")
 
 	for {
 		conn, err := listen.Accept()
 		if err != nil {
-			fmt.Printf("Failed to accept connection: %v\n", err)
+			log.Printf("Failed to accept connection: %v\n", err)
 			continue
 		}
 
@@ -30,10 +33,35 @@ func main() {
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
-	fmt.Printf("Received connection from %s\n", conn.RemoteAddr())
+	if err := handShakeDownlink(conn); err != nil {
+		log.Printf("Could not handshake: %v", err)
+		return
+	}
+
+	log.Printf("Received connection from %s", conn.RemoteAddr())
 
 	_, err := conn.Write([]byte("Hello, Client!\n"))
 	if err != nil {
-		fmt.Printf("Failed to send message to client: %v\n", err)
+		log.Printf("Failed to send message to client: %v\n", err)
+		return
 	}
+}
+
+func handShakeDownlink(conn net.Conn) error {
+	var clientVersion uint16
+
+	// Receive the version from client
+	if err := binary.Read(conn, binary.BigEndian, &clientVersion); err != nil {
+		return err
+	}
+
+	// Compare the version and send back the result
+	if clientVersion == ServerProtocolVersion {
+		_, err := conn.Write([]byte{1}) // Send true (as byte)
+		return err
+	}
+
+	log.Printf("Invalid client version %v", clientVersion)
+	_, err := conn.Write([]byte{0}) // Send false (as byte)
+	return err
 }
