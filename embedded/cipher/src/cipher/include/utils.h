71e1999153f768e3c46b04a0454efb1ad1974606
Mateo Segura <mateo@corekinect.com>
2023-09-15 21:16:22 +0000

    modified log macros to avoid annoying squiggles

--- embedded/cipher/src/cipher/include/utils.h
+++ embedded/cipher/src/cipher/include/utils.h
@@ -4,8 +4,8 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
-#define LOG(fmt, ...) LOG_INF("%s: " fmt, __func__, ##__VA_ARGS__)
-#define DBG(fmt, ...) LOG_DBG("%s: " fmt, __func__, ##__VA_ARGS__)
+#define LOG(fmt, ...) LOG_INF("%s", fmt, "", ##__VA_ARGS__)
+#define DBG(fmt, ...) LOG_DBG("%s", fmt, "", ##__VA_ARGS__)
 #define WARN(fmt, ...) LOG_WRN("%s: " fmt, __func__, ##__VA_ARGS__)
 #define ERROR(fmt, ...) LOG_ERR("%s: " fmt, __func__, ##__VA_ARGS__)
 
