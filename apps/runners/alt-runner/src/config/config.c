94a5a4973a0a0ec9b7e0a3f8fb9e1aef2179d945
Mateo Segura <mateo@corekinect.com>
2023-10-13 19:38:44 +0000

    added working initial version of runner

---
+++ apps/runners/alt-runner/src/config/config.c
@@ -0,0 +1,26 @@
+#include "config.h"
+
+#define REMOTE_HOST_IP "192.168.0.69"
+#define UPLINK_SOCKET 5001  // Client connects to this port
+
+static cipher_daemon_config_t cfg = {
+    .device_id = THIS_DEVICE_ID,
+    .uplink_ifaces[0] = {
+        .type = TAL_INTERFACE_TYPE_SOCKET,
+        .link = TAL_LINK_TYPE_UPLINK,
+        .host = REMOTE_HOST_IP,
+        .port = UPLINK_SOCKET,
+    },
+    .num_uplink_ifaces = 1,
+    .num_downlink_ifaces = 0,
+};
+
+static cipher_daemon_t daemon = {0};
+
+cipher_daemon_config_t* get_app_config(void) {
+    return &cfg;
+}
+
+cipher_daemon_t* get_app_daemon(void) {
+    return &daemon;
+}
---
+++ apps/runners/alt-runner/src/config/config.h
@@ -0,0 +1,12 @@
+#ifndef APP_CONFIG_H
+#define APP_CONFIG_H
+
+#include "config/daemon.h"
+#include "daemon/daemon.h"
+
+#define THIS_DEVICE_ID 125
+
+cipher_daemon_config_t* get_app_config(void);
+cipher_daemon_t* get_app_daemon(void);
+
+#endif
