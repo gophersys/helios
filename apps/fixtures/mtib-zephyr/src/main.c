911a25a494a43e7df7795666ad1341cfc37fdabb
Mateo Segura <mateo@corekinect.com>
2023-11-13 19:12:27 +0000

    added cipher to mtib zephyr app and workspace

--- apps/fixtures/mtib-zephyr/src/main.c
+++ apps/fixtures/mtib-zephyr/src/main.c
@@ -1,12 +1,23 @@
+// Standard includes
 #include <errno.h>
 #include <stdio.h>
 #include <string.h>
+
+// Zephyr includes
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+
+// Cipher includes
+#include "daemon/api.h"
 
 LOG_MODULE_REGISTER(app);
 
 int main(void) {
+    // Create a new daemon instance
+    cipher_daemon_config_t cfg = {};
+    cipher_daemon_t d = {};
+    cipher_daemon_init(&cfg, &d);
+
     while (1) {
         LOG_INF("Running");
         k_msleep(1000);
