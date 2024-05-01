38754201ef364f67a2c53cff2b1f5dbf065b0f2d
Mateo Segura <mateo@corekinect.com>
2024-05-01 16:59:32 -0600

    finalized healthcheck route

--- apps/fixtures/mtib_sigma5/proxy/src/api/v1/health/healthcheck.py
+++ apps/fixtures/mtib_sigma5/proxy/src/api/v1/health/healthcheck.py
@@ -1,22 +1,23 @@
-import subprocess
-import json
 import logging
-from flask import Blueprint, jsonify, make_response, request, has_request_context
-import concurrent.futures
+
+from flask import Blueprint
 
 healthcheck_bp = Blueprint('healthcheck', __name__)
 
-class SuppressHealthCheckLoggingFilter(logging.Filter):
+class HealthLogFilter(logging.Filter):
      """
      The healthcheck route is hit pretty frequently by clusters and clients alike,
      therefore we want to omitt the logs of this route being hit by filtering the 
      default logs from the Flask library
      """
      def filter(self, record):
-          if len(record.args) > 0 and record.args[0] == 'GET /v1/healthcheck HTTP/1.1':
+          if len(record.args) > 0 and record.args[0] == 'GET /v1/healthcheck HTTP/1.1': # This is the string that would normally be printed
                return False # Do not log if the path is the health check route
           return True
 
 @healthcheck_bp.route('/v1/healthcheck', methods=['GET'])
 def health_check():
+     """
+     This route just returns OK. Yes we're alive.
+     """
      return "", 200
