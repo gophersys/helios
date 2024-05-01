fb63d3c9e122c17fb8e0cfa38221f56ace484c7c
Mateo Segura <mateo@corekinect.com>
2024-05-01 17:02:50 -0600

    cleaned up main

--- apps/fixtures/mtib_sigma5/proxy/src/main.py
+++ apps/fixtures/mtib_sigma5/proxy/src/main.py
@@ -6,60 +6,53 @@ from flask import Flask
 
 # App includes
 from config import conf
+from src.services.proxy import appProxyServer, ProxyServerConfiguration
 
-# Routes
+# Server
+server = Flask(__name__) 
 
-# from api.v1.cluster.create import cluster_create_bp
-# from api.v1.cluster.read import cluster_read_bp
-# from api.v1.cluster.update import cluster_update_bp
-# from api.v1.cluster.delete import cluster_delete_bp
-# from api.v1.cluster.register import cluster_register_bp
+# ----------------------------------------------------------------------------------
+#                                                                             Routes
+# --------------------------------------------------------------------------------*/
+from api.v1.health.healthcheck import (                                                 
+    healthcheck_bp,                                                                     # POST /v1/healthcheck
+    HealthLogFilter
+    )
+server.register_blueprint(healthcheck_bp)
 
-# from api.v1.cluster.deployment.get import cluster_get_deployment_bp
+from api.v1.clusters.create import clusters_create_bp                                   # POST /v1/clusters
+server.register_blueprint(clusters_create_bp)
 
-# from api.v1.cluster.tests.list import cluster_tests_list_bp
-# -------------------------------------------------------------------------------------------------
-#                                                                                       HTTP Server
-# -----------------------------------------------------------------------------------------------*/
-app = Flask(__name__)
+from api.v1.clusters.list import clusters_list_bp                                       # GET /v1/clusters
+server.register_blueprint(clusters_list_bp)
 
-from api.v1.health.healthcheck import healthcheck_bp, SuppressHealthCheckLoggingFilter
-app.register_blueprint(healthcheck_bp)
+from api.v1.clusters.delete_all import clusters_delete_all_bp                           # DELETE /v1/clusters
+server.register_blueprint(clusters_delete_all_bp)
 
-from api.v1.clusters.create import clusters_create_bp
-app.register_blueprint(clusters_create_bp)
+from api.v1.clusters.delete_uuid import clusters_delete_uuid_bp                         # DELETE /v1/clusters/<uuid>
+server.register_blueprint(clusters_delete_uuid_bp)
 
-from api.v1.clusters.list import clusters_list_bp
-app.register_blueprint(clusters_list_bp)
+from api.v1.clusters.register_uuid import clusters_register_uuid_bp                     # POST /v1/clusters/<uuid>/register
+server.register_blueprint(clusters_register_uuid_bp)
 
-from api.v1.clusters.delete import clusters_delete_bp
-app.register_blueprint(clusters_delete_bp)
-
-from api.v1.clusters.delete_uuid import clusters_delete_uuid_bp
-app.register_blueprint(clusters_delete_uuid_bp)
-
-
-# app.register_blueprint(cluster_create_bp)
-# app.register_blueprint(cluster_read_bp)
-# app.register_blueprint(cluster_update_bp)
-# app.register_blueprint(cluster_delete_bp)
-# app.register_blueprint(cluster_register_bp)
-
-# app.register_blueprint(cluster_get_deployment_bp)
-
-# app.register_blueprint(cluster_tests_list_bp)
-
-
-# -------------------------------------------------------------------------------------------------
-#                                                                                              Main
-# -----------------------------------------------------------------------------------------------*/
+# ----------------------------------------------------------------------------------
+#                                                                              Entry 
+# --------------------------------------------------------------------------------*/
 if __name__ == '__main__':
     logging.debug(f"App configuration: \n{conf}")
     
     # Remove healthcheck route hits from logs
     logger = logging.getLogger('werkzeug')
-    logger.addFilter(SuppressHealthCheckLoggingFilter())
+    logger.addFilter(HealthLogFilter()) # <- More info here
+    
+    # Instantiate server with desired configuration
+    app_config:ProxyServerConfiguration = ProxyServerConfiguration(
+        db_storage_path=conf.DB_STORAGE_PATH,
+        db_storage_limit_gb=conf.DB_STORAGE_LIMIT_GB,
+        supported_registries=conf.SUPPORTED_REGISTRIES,
+    )
+    appProxyServer.init(app_config)
     
     # Start the server
-    app.run(host='0.0.0.0', port=conf.SERVER_PORT, debug=False)
+    server.run(host='0.0.0.0', port=conf.SERVER_PORT, debug=False)
     
