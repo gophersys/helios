8311118f7543fd79c79893ca2841d6bf966f7525
Mateo Segura <mateo@corekinect.com>
2024-05-08 14:39:35 -0600

    extended operator protocol for support pods and nodes info

--- libs/protos/cluster_operator/cluster_operator_pb2.py
+++ libs/protos/cluster_operator/cluster_operator_pb2.py
@@ -15,15 +15,15 @@ _sym_db = _symbol_database.Default()
 import protos.cluster_test.cluster_test_pb2 as cluster__test__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x16\x63luster_operator.proto\x12\x10\x63luster_operator\x1a\x12\x63luster_test.proto\"\x14\n\x12HealthCheckRequest\"U\n\x13HealthCheckResponse\x12/\n\x06status\x18\x01 \x01(\x0e\x32\x1f.cluster_operator.ClusterStatus\x12\r\n\x05\x65rror\x18\x02 \x01(\t\"\xdd\x01\n\x08NodeInfo\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x10\n\x08hostname\x18\x02 \x01(\t\x12\x10\n\x08os_image\x18\x03 \x01(\t\x12\x16\n\x0ekernel_version\x18\x04 \x01(\t\x12\x11\n\tcpu_cores\x18\x05 \x01(\x03\x12\x19\n\x11memory_used_bytes\x18\x06 \x01(\x03\x12\x1d\n\x15memory_capacity_bytes\x18\x07 \x01(\x03\x12\x1a\n\x12storage_used_bytes\x18\x08 \x01(\x03\x12\x1e\n\x16storage_capacity_bytes\x18\t \x01(\x03\"\x17\n\x15GetClusterInfoRequest\"H\n\x16GetClusterInfoResponse\x12.\n\nnodes_info\x18\x01 \x03(\x0b\x32\x1a.cluster_operator.NodeInfo\"\x1e\n\x0e\x44\x65ploymentInfo\x12\x0c\n\x04name\x18\x01 \x01(\t\"\x1a\n\x18GetDeploymentInfoRequest\"K\n\x19GetDeploymentInfoResponse\x12.\n\x04info\x18\x01 \x01(\x0b\x32 .cluster_operator.DeploymentInfo\"I\n\x13RegisterTestRequest\x12\x0c\n\x04port\x18\x01 \x01(\x05\x12$\n\x04info\x18\x02 \x01(\x0b\x32\x16.cluster_test.TestInfo\"6\n\x14RegisterTestResponse\x12\x0f\n\x07success\x18\x01 \x01(\x08\x12\r\n\x05\x65rror\x18\x02 \x01(\t\"\x12\n\x10ListTestsRequest\":\n\x11ListTestsResponse\x12%\n\x05tests\x18\x01 \x03(\x0b\x32\x16.cluster_test.TestInfo*Z\n\rClusterStatus\x12\x0c\n\x08STARTING\x10\x00\x12\t\n\x05READY\x10\x01\x12\x0c\n\x08UPDATING\x10\x02\x12\x08\n\x04IDLE\x10\x03\x12\x0b\n\x07RUNNING\x10\x04\x12\x0b\n\x07\x45RRORED\x10\x05\x32\xe4\x04\n\x0f\x43lusterOperator\x12\\\n\x0bHealthCheck\x12$.cluster_operator.HealthCheckRequest\x1a%.cluster_operator.HealthCheckResponse\"\x00\x12\x65\n\x0eGetClusterInfo\x12\'.cluster_operator.GetClusterInfoRequest\x1a(.cluster_operator.GetClusterInfoResponse\"\x00\x12n\n\x11GetDeploymentInfo\x12*.cluster_operator.GetDeploymentInfoRequest\x1a+.cluster_operator.GetDeploymentInfoResponse\"\x00\x12_\n\x0cRegisterTest\x12%.cluster_operator.RegisterTestRequest\x1a&.cluster_operator.RegisterTestResponse\"\x00\x12V\n\tListTests\x12\".cluster_operator.ListTestsRequest\x1a#.cluster_operator.ListTestsResponse\"\x00\x12\x63\n\x18\x45xecuteManufacturingTest\x12 .cluster_test.ExecuteTestRequest\x1a!.cluster_test.ExecuteTestResponse\"\x00\x30\x01\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x16\x63luster_operator.proto\x12\x10\x63luster_operator\x1a\x12\x63luster_test.proto\"\x14\n\x12HealthCheckRequest\"U\n\x13HealthCheckResponse\x12/\n\x06status\x18\x01 \x01(\x0e\x32\x1f.cluster_operator.ClusterStatus\x12\r\n\x05\x65rror\x18\x02 \x01(\t\"\xdd\x01\n\x08NodeInfo\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x10\n\x08hostname\x18\x02 \x01(\t\x12\x10\n\x08os_image\x18\x03 \x01(\t\x12\x16\n\x0ekernel_version\x18\x04 \x01(\t\x12\x11\n\tcpu_cores\x18\x05 \x01(\x03\x12\x19\n\x11memory_used_bytes\x18\x06 \x01(\x03\x12\x1d\n\x15memory_capacity_bytes\x18\x07 \x01(\x03\x12\x1a\n\x12storage_used_bytes\x18\x08 \x01(\x03\x12\x1e\n\x16storage_capacity_bytes\x18\t \x01(\x03\"\x17\n\x15GetClusterInfoRequest\"H\n\x16GetClusterInfoResponse\x12.\n\nnodes_info\x18\x01 \x03(\x0b\x32\x1a.cluster_operator.NodeInfo\"e\n\x07PodInfo\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\r\n\x05image\x18\x02 \x01(\t\x12\x0c\n\x04node\x18\x03 \x01(\t\x12\x0e\n\x06status\x18\x04 \x01(\t\x12\x10\n\x08restarts\x18\x05 \x01(\x05\x12\r\n\x05ready\x18\x06 \x01(\x08\"Z\n\x0e\x44\x65ploymentInfo\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x0c\n\x04uuid\x18\x02 \x01(\t\x12,\n\tpods_info\x18\x03 \x03(\x0b\x32\x19.cluster_operator.PodInfo\"\x1a\n\x18GetDeploymentInfoRequest\"V\n\x19GetDeploymentInfoResponse\x12\x39\n\x0f\x64\x65ployment_info\x18\x01 \x03(\x0b\x32 .cluster_operator.DeploymentInfo\"I\n\x13RegisterTestRequest\x12\x0c\n\x04port\x18\x01 \x01(\x05\x12$\n\x04info\x18\x02 \x01(\x0b\x32\x16.cluster_test.TestInfo\"6\n\x14RegisterTestResponse\x12\x0f\n\x07success\x18\x01 \x01(\x08\x12\r\n\x05\x65rror\x18\x02 \x01(\t\"\x12\n\x10ListTestsRequest\":\n\x11ListTestsResponse\x12%\n\x05tests\x18\x01 \x03(\x0b\x32\x16.cluster_test.TestInfo*Z\n\rClusterStatus\x12\x0c\n\x08STARTING\x10\x00\x12\t\n\x05READY\x10\x01\x12\x0c\n\x08UPDATING\x10\x02\x12\x08\n\x04IDLE\x10\x03\x12\x0b\n\x07RUNNING\x10\x04\x12\x0b\n\x07\x45RRORED\x10\x05\x32\xe4\x04\n\x0f\x43lusterOperator\x12\\\n\x0bHealthCheck\x12$.cluster_operator.HealthCheckRequest\x1a%.cluster_operator.HealthCheckResponse\"\x00\x12\x65\n\x0eGetClusterInfo\x12\'.cluster_operator.GetClusterInfoRequest\x1a(.cluster_operator.GetClusterInfoResponse\"\x00\x12n\n\x11GetDeploymentInfo\x12*.cluster_operator.GetDeploymentInfoRequest\x1a+.cluster_operator.GetDeploymentInfoResponse\"\x00\x12_\n\x0cRegisterTest\x12%.cluster_operator.RegisterTestRequest\x1a&.cluster_operator.RegisterTestResponse\"\x00\x12V\n\tListTests\x12\".cluster_operator.ListTestsRequest\x1a#.cluster_operator.ListTestsResponse\"\x00\x12\x63\n\x18\x45xecuteManufacturingTest\x12 .cluster_test.ExecuteTestRequest\x1a!.cluster_test.ExecuteTestResponse\"\x00\x30\x01\x62\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'cluster_operator_pb2', _globals)
 if _descriptor._USE_C_DESCRIPTORS == False:
   DESCRIPTOR._options = None
-  _globals['_CLUSTERSTATUS']._serialized_start=844
-  _globals['_CLUSTERSTATUS']._serialized_end=934
+  _globals['_CLUSTERSTATUS']._serialized_start=1018
+  _globals['_CLUSTERSTATUS']._serialized_end=1108
   _globals['_HEALTHCHECKREQUEST']._serialized_start=64
   _globals['_HEALTHCHECKREQUEST']._serialized_end=84
   _globals['_HEALTHCHECKRESPONSE']._serialized_start=86
@@ -34,20 +34,22 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _globals['_GETCLUSTERINFOREQUEST']._serialized_end=420
   _globals['_GETCLUSTERINFORESPONSE']._serialized_start=422
   _globals['_GETCLUSTERINFORESPONSE']._serialized_end=494
-  _globals['_DEPLOYMENTINFO']._serialized_start=496
-  _globals['_DEPLOYMENTINFO']._serialized_end=526
-  _globals['_GETDEPLOYMENTINFOREQUEST']._serialized_start=528
-  _globals['_GETDEPLOYMENTINFOREQUEST']._serialized_end=554
-  _globals['_GETDEPLOYMENTINFORESPONSE']._serialized_start=556
-  _globals['_GETDEPLOYMENTINFORESPONSE']._serialized_end=631
-  _globals['_REGISTERTESTREQUEST']._serialized_start=633
-  _globals['_REGISTERTESTREQUEST']._serialized_end=706
-  _globals['_REGISTERTESTRESPONSE']._serialized_start=708
-  _globals['_REGISTERTESTRESPONSE']._serialized_end=762
-  _globals['_LISTTESTSREQUEST']._serialized_start=764
-  _globals['_LISTTESTSREQUEST']._serialized_end=782
-  _globals['_LISTTESTSRESPONSE']._serialized_start=784
-  _globals['_LISTTESTSRESPONSE']._serialized_end=842
-  _globals['_CLUSTEROPERATOR']._serialized_start=937
-  _globals['_CLUSTEROPERATOR']._serialized_end=1549
+  _globals['_PODINFO']._serialized_start=496
+  _globals['_PODINFO']._serialized_end=597
+  _globals['_DEPLOYMENTINFO']._serialized_start=599
+  _globals['_DEPLOYMENTINFO']._serialized_end=689
+  _globals['_GETDEPLOYMENTINFOREQUEST']._serialized_start=691
+  _globals['_GETDEPLOYMENTINFOREQUEST']._serialized_end=717
+  _globals['_GETDEPLOYMENTINFORESPONSE']._serialized_start=719
+  _globals['_GETDEPLOYMENTINFORESPONSE']._serialized_end=805
+  _globals['_REGISTERTESTREQUEST']._serialized_start=807
+  _globals['_REGISTERTESTREQUEST']._serialized_end=880
+  _globals['_REGISTERTESTRESPONSE']._serialized_start=882
+  _globals['_REGISTERTESTRESPONSE']._serialized_end=936
+  _globals['_LISTTESTSREQUEST']._serialized_start=938
+  _globals['_LISTTESTSREQUEST']._serialized_end=956
+  _globals['_LISTTESTSRESPONSE']._serialized_start=958
+  _globals['_LISTTESTSRESPONSE']._serialized_end=1016
+  _globals['_CLUSTEROPERATOR']._serialized_start=1111
+  _globals['_CLUSTEROPERATOR']._serialized_end=1723
 # @@protoc_insertion_point(module_scope)
