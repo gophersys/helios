a7d8c804bfef3d955441e8ce16173064fdd5b81f
Mateo Segura <mateo.segura413@gmail.com>
2026-07-05 21:28:33 -0700

    chore: recover cipher from backups (bitbucket access revoked)
    cipher: from concord .deprecated copy (only surviving source)
    iface: from iface.zip (full lib + samples)

---
+++ zephyr/include/daemon/fifo.h
@@ -0,0 +1,48 @@
+#ifndef FIFO_H
+#define FIFO_H
+
+// Zephyr includes
+#include "zephyr/kernel.h"
+
+// Cipher includes
+#include <corekinect/cipher/config.h>
+#include <corekinect/cipher/iface.h>
+#include <corekinect/cipher/ops/entry.h>
+#include <corekinect/cipher/ops/event.h>
+#include <corekinect/cipher/ops/rpc.h>
+#include <corekinect/cipher/ops/stream.h>
+#include <corekinect/cipher/ops/types.h>
+#include <corekinect/cipher/protocol.h>
+#include <corekinect/tal.h>
+
+#include "config/default.h"
+#include "daemon/daemon.h"
+#include "daemon/service.h"
+
+typedef struct {
+    uintptr_t __k_reserved;
+    cipher_packet_t packet;
+    cipher_iface_t *iface;
+} cipher_packet_fifo_item_t;
+
+typedef struct {
+    uintptr_t __k_reserved;
+    uint8_t *raw_packet;
+    size_t packet_len;
+} cipher_router_packet_fifo_item_t;
+
+typedef struct {
+    uintptr_t __k_reserved;
+    cipher_registry_rpc_entry_t *entry;
+} cipher_local_rpc_request_fifo_item_t;
+
+cipher_packet_fifo_item_t *alloc_packet_fifo_item(cipher_daemon_t *d, size_t payload_size);
+void free_packet_fifo_item(cipher_daemon_t *d, cipher_packet_fifo_item_t *fifo_item);
+
+cipher_router_packet_fifo_item_t *alloc_router_packet_fifo_item(cipher_daemon_t *d, size_t packet_size);
+void free_router_packet_fifo_item(cipher_daemon_t *d, cipher_router_packet_fifo_item_t *fifo_item);
+
+cipher_local_rpc_request_fifo_item_t *alloc_local_rpc_request_fifo_item(cipher_daemon_t *d, size_t req_size, size_t rep_size);
+void free_local_rpc_request_fifo_item(cipher_daemon_t *d, cipher_local_rpc_request_fifo_item_t *fifo_item);
+
+#endif
---
+++ zephyr/src/include/events.h
@@ -0,0 +1,64 @@
+#ifndef CIPHER_PRIV_H
+#define CIPHER_PRIV_H
+
+// Standard includes
+#include <stdbool.h>
+#include <stdint.h>
+
+// Cipher includes
+#include "daemon/daemon.h"
+
+/*-----------------------------------------------------------------------------------------------------
+ *                                                                                                Types
+ *---------------------------------------------------------------------------------------------------*/
+typedef enum {
+    CTRL_EVENT_TYPE_IFACE_CONNECTED,
+    CTRL_EVENT_TYPE_IFACE_DISCONNECTED,
+    CTRL_EVENT_TYPE_EXIT,
+
+    CTRL_EVENT_TYPE_MAX,
+} ctrl_event_type_t;
+
+typedef struct {
+    cipher_iface_t *iface;
+} ctrl_event_opt_iface_conn_t;
+
+typedef struct {
+    uintptr_t __k_reserved;
+
+    ctrl_event_type_t type;
+    uint16_t id;
+    void *options;
+} ctrl_event_t;
+
+/*-----------------------------------------------------------------------------------------------------
+ *                                                                                                  API
+ *---------------------------------------------------------------------------------------------------*/
+void cipher_ctrl_add_event(cipher_daemon_t *d, ctrl_event_t *event);
+
+typedef enum {
+    RPC_EVENT_TYPE_IFACE_DISCONNECTED,
+    RPC_EVENT_TYPE_TIMER_EXPIRED,
+
+    RPC_EVENT_TYPE_MAX,
+} rpc_event_type_t;
+
+typedef struct {
+    cipher_iface_t *iface;
+} rpc_event_opt_iface_disconn_t;
+
+typedef struct {
+    struct k_timer *timer;
+} rpc_event_opt_timer_expired_t;
+
+typedef struct {
+    uintptr_t __k_reserved;
+
+    rpc_event_type_t type;
+    uint16_t id;
+    void *options;
+} rpc_event_t;
+
+void cipher_rpc_thread_add_event(cipher_daemon_t *d, rpc_event_t *event);
+
+#endif
